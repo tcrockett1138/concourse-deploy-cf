e3885603a40cae7e16af2f652e12c96360478125
Tim Crockett <tim.crockett@gmail.com>
2017-10-11 16:30:44 -0600

    before first deploy

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -e
 
-export SYSTEM_DOMAIN=neogrid.cf-training.net
-
-bosh -n d cf cf-deployment/cf-deployment.yml -o opsfiles/scale-to-one-az.yml --vars-store=creds.yml -v system_domain=$SYSTEM_DOMAIN
+bosh -n d cf cf-deployment/cf-deployment.yml -o cf-deployment/operations/scale-to-one-az.yml --vars-store=credentials/creds.yml -v system_domain=$SYSTEM_DOMAIN
 
--- ci/tasks/upload-release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-#bosh -d cf -n ur https://54.71.169.139:25555
-bosh -d cf -n us https://s3.amazonaws.com/bosh-aws-light-stemcells/light-bosh-stemcell-3468-aws-xen-hvm-ubuntu-trusty-go_agent.tgz
