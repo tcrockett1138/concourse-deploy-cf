c2672ddf8e1546b3d85d79a568ec069c993d29ba
Tim Crockett <tim.crockett@gmail.com>
2017-10-11 18:09:42 -0600

    fix redundant deploy name

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-bosh -n d cf cf-deployment/cf-deployment.yml -o cf-deployment/operations/scale-to-one-az.yml --vars-store=credentials/credentials.yml -v system_domain=$SYSTEM_DOMAIN
+bosh -n deploy cf-deployment/cf-deployment.yml -o cf-deployment/operations/scale-to-one-az.yml --vars-store=credentials/credentials.yml -v system_domain=$SYSTEM_DOMAIN
 
