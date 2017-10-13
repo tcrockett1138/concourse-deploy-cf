a1e6ecfbe8d63abf94d5d71dfbe32b80fa479b42
Tim Crockett <tim.crockett@gmail.com>
2017-10-13 14:09:56 -0600

    fix typo in mysql.yml path

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-bosh -n deploy cf-deployment/cf-deployment.yml -o cf-deployment/operations/scale-to-one-az.yml -o concourse-deploy-cf/ci/opsfiles --vars-store=credentials/credentials.yml -v system_domain=$SYSTEM_DOMAIN
+bosh -n deploy cf-deployment/cf-deployment.yml -o cf-deployment/operations/scale-to-one-az.yml -o concourse-deploy-cf/ci/opsfiles/mysql.yml --vars-store=credentials/credentials.yml -v system_domain=$SYSTEM_DOMAIN
