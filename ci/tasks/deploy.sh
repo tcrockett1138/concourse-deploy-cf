0b1d7bae0d0e64a3da732f311a75374c379ed347
Tim Crockett <tim.crockett@gmail.com>
2017-10-13 12:42:33 -0600

    add mysql opsfile to build

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-bosh -n deploy cf-deployment/cf-deployment.yml -o cf-deployment/operations/scale-to-one-az.yml --vars-store=credentials/credentials.yml -v system_domain=$SYSTEM_DOMAIN
+bosh -n deploy cf-deployment/cf-deployment.yml -o cf-deployment/operations/scale-to-one-az.yml -o cf-deployment/operations/mysql.yml --vars-store=credentials/credentials.yml -v system_domain=$SYSTEM_DOMAIN
 
